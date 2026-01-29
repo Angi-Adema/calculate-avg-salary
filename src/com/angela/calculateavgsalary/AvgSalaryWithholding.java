e51e85e5cd938388adb90e45dc1b92a832b96aa1
Angi-Adema <dr.adema@gmail.com>
2026-01-28 22:11:40 -0700

    completed the if-else statement calculating each required tax bracket and outputting the mount of tax withheld each week as well as the total net weekly salary.

--- src/com/angela/calculateavgsalary/AvgSalaryWithholding.java
+++ src/com/angela/calculateavgsalary/AvgSalaryWithholding.java
@@ -3,6 +3,7 @@ package com.angela.calculateavgsalary;
 import java.util.Scanner;
 
 public class AvgSalaryWithholding {
+    
     public static void main(String[] args) {
         
         // Pseudocode:
@@ -27,26 +28,36 @@ public class AvgSalaryWithholding {
         System.out.println("Please enter your weekly salary amount before tax withholding: ");
         weeklyGrossSalary = input.nextInt();
         
-        // Calculate tax amount withheld and net salary based on gross salary amount
+        // Calculate tax amount withheld and net salary for salary less than $500
         if (weeklyGrossSalary < 500) {
             taxAmount = weeklyGrossSalary * 0.10;
             weeklyNetSalary = weeklyGrossSalary - taxAmount;
             System.out.printf("Tax Amount Withheld: %.2f%n", + taxAmount);
             System.out.printf("Weekly Net Salary: %.2f%n",  weeklyNetSalary);
-        } 
-        else if (weeklyGrossSalary < 1500) {
+        }
+        // Calculate tax amount withheld and net salary for salary between $500 and $1500 
+        else if ((weeklyGrossSalary >= 500) && (weeklyGrossSalary < 1500)) {
             taxAmount = weeklyGrossSalary * 0.15;
             weeklyNetSalary = weeklyGrossSalary - taxAmount;
             System.out.printf("Tax Amount Withheld: %.2f%n", + taxAmount);
             System.out.printf("Weekly Net Salary: %.2f%n",  weeklyNetSalary);
         } 
-        else if (weeklyGrossSalary < 2500) {
+        // Calculate tax amount withheld and net salary for salary between $1500 and $2500
+        else if ((weeklyGrossSalary >= 1500) && (weeklyGrossSalary < 2500)) {
             taxAmount = weeklyGrossSalary * 0.20;
             weeklyNetSalary = weeklyGrossSalary - taxAmount;
             System.out.printf("Tax Amount Withheld: %.2f%n", + taxAmount);
             System.out.printf("Weekly Net Salary: %.2f%n",  weeklyNetSalary);
         }
+        // Calculate tax amount withheld and net salary for salary $2500 and above
+        else {
+            taxAmount = weeklyGrossSalary * 0.30;
+            weeklyNetSalary = weeklyGrossSalary - taxAmount;
+            System.out.printf("Tax Amount Withheld: %.2f%n", + taxAmount);
+            System.out.printf("Weekly Net Salary: %.2f%n",  weeklyNetSalary);
+        }
 
+        // Close the scanner
         input.close();
     }
 }
